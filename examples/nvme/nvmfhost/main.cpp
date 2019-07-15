7231307a64122e6050f9f0611f1a4f01d26a1864
gwnet <gw_net@163.com>
2019-07-15 11:00:00 -0700

    upload nvmfhost project

---
+++ examples/nvme/nvmfhost/main.cpp
@@ -0,0 +1,49 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <memory>
+
+#include <rte_lcore.h>
+#include <spdk/env.h>
+#include <spdk/nvme.h>
+#include <spdk/nvme_intel.h>
+#include <spdk/queue.h>
+#include <spdk/stdinc.h>
+#include <spdk/util.h>
+
+
+
+using namespace spdk;
+using namespace nvmfhost;
+using namespace std;
+
+
+
+int main(int argc, char **argv) {
+  int ret;
+  uint32_t master_core;
+  struct spdk_env_opts opts;
+
+  
+  spdk_env_opts_init(&opts);
+  opts.name = "nvmfhost";
+  opts.core_mask = "0xc";
+  if (spdk_env_init(&opts) < 0) {
+	  std::cout << "spdk_env_init failed";
+    return -1;
+  }
+  master_core = spdk_env_get_current_core();
+  std::cout << "SPDK master core id: " << master_core;
+
+
+  return 0;
+}
+
